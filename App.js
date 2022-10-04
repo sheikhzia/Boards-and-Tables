0c77881ddbb1b07962825a8be2ba59aaf0d3ff32
sheikhzia <114021361+sheikhzia@users.noreply.github.com>
2022-10-04 23:10:46 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar";
+import Upload from "./upload";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar/>
+      <Upload/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+      <div className="App">
+    <nav>         
+        <p className="Logo">
+            Logo
+        </p>
+        <h4 className="Ham1"></h4>      
+        <h4 className="Ham2"></h4>      
+        <h4 className="Ham3"></h4>      
+    </nav>
+    <div className="Paragraph">
+    <h2 className="Start">Board Minutes</h2>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
